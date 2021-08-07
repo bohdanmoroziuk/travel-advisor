b23ea59e89861bf15bacb8b07fc67a8f785e0765
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-08-07 08:53:18 +0300

    chore(deps): add material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'index.css';
+import '@fontsource/roboto';
+
 import App from 'App';
 import reportWebVitals from 'reportWebVitals';
 
