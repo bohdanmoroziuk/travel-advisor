5bc9bce529a671280d6f966e9e4897fed50d31b9
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-08-11 21:24:15 +0300

    feat(map): change height

--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -1,8 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
-  mapContainer: {
-    height: '85vh', 
+  mapContainer: { 
+    height: 'calc(100vh - 64px)',
     width: '100%',
   },
 }));
