93a4f24965ee250e10751905435da9e3a961442b
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-08-11 21:06:45 +0300

    chore: add config object

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+  googleMapApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+  travelAdvisorApiKey: process.env.REACT_APP_RAPIDAPI_TRAVEL_ADVISOR_KEY,
+  travelAdvisorApiHost: process.env.REACT_APP_RAPIDAPI_TRAVEL_ADVISOR_HOST,
+  openWeatherMapApiKey: process.env.REACT_APP_RAPIDAPI_OPEN_WEATHER_MAP_KEY,
+  openWeatherMapApiHost: process.env.REACT_APP_RAPIDAPI_OPEN_WEATHER_MAP_HOST,
+};
--- src/services/places.ts
+++ src/services/places.ts
@@ -2,14 +2,13 @@ import axios from 'axios';
 import { nanoid } from 'nanoid';
 import { Coords } from 'google-map-react';
 
-const key = process.env.REACT_APP_RAPIDAPI_KEY;
-const host = process.env.REACT_APP_RAPIDAPI_HOST;
+import config from 'config';
 
 const http = axios.create({
   baseURL: 'https://travel-advisor.p.rapidapi.com',
   headers: {
-    'x-rapidapi-key': key,
-    'x-rapidapi-host': host,
+    'x-rapidapi-key': config.travelAdvisorApiKey,
+    'x-rapidapi-host': config.travelAdvisorApiHost,
   },
 });
 
